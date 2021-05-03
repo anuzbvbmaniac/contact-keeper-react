8d66e4f07998c411f428f0ff6ae4b138834e17ba
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2021-05-03 19:13:05 +0545

    Changes: User Save API Complete.

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
-const user = require('../models/User');
+const User = require('../models/User');
 
 /**
  *  @route   POST api/users
@@ -13,15 +14,48 @@ router.post('/', [
     check('name', 'Please add your name.').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
-], (request, response) => {
+], async (request, response) => {
+
     const errors = validationResult(request);
+
     if (!errors.isEmpty()) {
         return response.status(400).json({
             errors: errors.array()
         });
     }
 
-    response.send('Passed');
+    const { name, email, password } = request.body;
+
+    try {
+
+        let user = await User.findOne({ email }); // !ES6 ? email: email
+
+        if (user) {
+            return response.status(400).json({
+                msg: "User already exists."
+            });
+        }
+
+        // Create a new Instance of the User.
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        response.send('User Saved');
+
+    } catch (err) {
+        console.error(err.message);
+        response.status(500).send('Server is hot. Please try again later.');
+    }
+
 });
 
 module.exports = router;

b9cab294ec485452a287b8a1b8a1698361d5e239
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2021-05-03 21:46:36 +0545

    Changes: Login API Endpoint. 1. Log User with [Email, Password]. 2. Check for email and password. Authenticate them and return response (Error message{error} || token {success}).

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
 const router = express.Router();
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
 
 /**
  *  @route   GET api/auth
@@ -16,17 +22,43 @@ router.get('/', (request, response) => {
  *  @desc    Auth User and Get token
  *  @access  Public
  */
-router.post('/', (request, response) => {
-    response.send('Log in User.');
-});
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please type your password.').exists(),
+], async (request, response) => {
 
-/**
- *  @route   POST api/auth
- *  @desc    Auth User and Get token
- *  @access  Public
- */
-router.post('/', (request, response) => {
-    response.send('Log in User.');
+    const errors = validationResult(request);
+
+    if (!errors) return response.status(400).json({ errors: errors.array() });
+
+    const { email, password } = request.body;
+
+    try {
+
+        let user = await User.findOne({ email });
+
+        if (!user) return response.status(400).json({ msg: 'Invalid email address. User not found.' });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) return response.status(400).json({ msg: 'Invalid password. Please check your password.' });
+
+        const payload = {
+            user: { id: user.id }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000 // set to 3600 on production
+        }, (error, token) => {
+            if (error) throw error;
+            response.json({ token })
+        });
+
+    } catch (err) {
+        console.error(err.message);
+        response.status(500).send('Server is hot. Please try again later.');
+    }
+
 });
 
 module.exports = router;

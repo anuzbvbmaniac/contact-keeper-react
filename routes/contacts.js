ef17f92ecd9d184b274186654ac9bfc56a8f9927
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2021-05-03 23:59:52 +0545

    Changes: Create, Update Delete Contact. 1. Create Contact Api Endpoint Complete. 2. Update Contact Api Endpoint Complete. 3. Delete Contact Api Endpoint Complete. 4. docs/ folder has POSTMAN JSON api collection for future references.

--- routes/contacts.js
+++ routes/contacts.js
@@ -28,8 +28,31 @@ router.get('/', auth, async (request, response) => {
  *  @desc    Add New contact
  *  @access  Private
  */
-router.post('/', (request, response) => {
-    response.send('Add new Contact');
+router.post('/', [auth, [
+    check('name', 'Name is required. Please enter the contact name.').not().isEmpty()
+]], async (request, response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) return response.status(400).json({ errors: errors.array() });
+
+    const { name, email, phone, type } = request.body;
+
+    try {
+
+        const initContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: request.user.id,
+        });
+
+        const contact = await initContact.save();
+
+        response.json(contact);
+    } catch (err) {
+        console.error(err.message);
+        response.status(500).send('Server is hot. Please try again later.');
+    }
 });
 
 /**
@@ -37,8 +60,33 @@ router.post('/', (request, response) => {
  *  @desc    Update contact
  *  @access  Private
  */
-router.put('/:id', (request, response) => {
-    response.send('Update Contact');
+router.put('/:id', auth, async (request, response) => {
+
+    const { name, email, phone, type } = request.body;
+
+    const contactFields = {};
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+    if (type) contactFields.type = type;
+
+    try {
+        let contact = await Contact.findById(request.params.id);
+
+        if (!contact) return response.status(404).json({ msg: 'Contact not found. PLease check you url.' });
+
+        // Check id the Contact Data is of logged in User or not.
+        if (contact.user.toString() !== request.user.id) return response.status(401).json({ msg: 'Unauthorized. You cannot change this contact information.' });
+
+        contact = await Contact.findByIdAndUpdate(request.params.id,
+            { $set: contactFields },
+            { new: true });
+
+        response.json(contact);
+    } catch (err) {
+        console.error(err.message);
+        response.status(500).send('Server is hot. Please try again later.');
+    }
 });
 
 /**
@@ -46,8 +94,22 @@ router.put('/:id', (request, response) => {
  *  @desc    Delete contact
  *  @access  Private
  */
-router.delete('/:id', (request, response) => {
-    response.send('Delete Contact');
+router.delete('/:id', auth, async (request, response) => {
+    try {
+        let contact = await Contact.findById(request.params.id);
+
+        if (!contact) return response.status(404).json({ msg: 'Contact not found. PLease check you url.' });
+
+        // Check id the Contact Data is of logged in User or not.
+        if (contact.user.toString() !== request.user.id) return response.status(401).json({ msg: 'Unauthorized. You cannot change this contact information.' });
+
+        await Contact.findByIdAndRemove(request.params.id);
+
+        response.json({msg: 'Contact removed successfully.'});
+    } catch (err) {
+        console.error(err.message);
+        response.status(500).send('Server is hot. Please try again later.');
+    }
 });
 
 module.exports = router;
